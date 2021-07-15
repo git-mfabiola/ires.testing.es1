75dc3acbebe9ec91972e16ffc66e87de8c87bd45
git-mfabiola <fabiola.majcen@gmail.com>
2021-07-15 10:29:27 +0200

    Calculator init

---
+++ src/main/java/it/euris/ires/CalculatorMain.java
@@ -0,0 +1,13 @@
+package it.euris.ires;
+
+public class CalculatorMain {
+
+    public static void main(String[] args) {
+        Calculator test1 = new Calculator();
+        int a=1;
+        int b= 1;
+        int somma = a + b;
+        System.out.println(somma);
+        }
+    }
+
